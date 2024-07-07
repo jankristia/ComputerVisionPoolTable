fce63022c8bdbaa65900ed61de8a9779f451fa08
Jan Kristian Alstergren <jankal@stud.ntnu.no>
2024-07-07 11:10:45 +0200

    Hello world! Able to show video given its relative path

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ComputerVisionPoolTable
+
+Program that detects the table and balls of a pool-table.
+
+To run the program:
+- Delete the content of the build-folder
+- Run cmake.. from the build-folder
+- Run make in the build-folder
+- Run ./finalProject <path to video>
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char** argv) {
+
+    if(argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <imageDirectory path> " << std::endl;
+        return -1;
+    }
+
+    std::string imageDirectory = argv[1];
+
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    cv::VideoCapture cap(imageDirectory); 
+
+    // Check if camera opened successfully
+    if(!cap.isOpened()){
+        std::cout << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
+
+    while(1){
+
+    cv::Mat frame;
+    
+    // Capture frame-by-frame
+    cap >> frame;
+
+    // If the frame is empty, break immediately
+    if (frame.empty()) {
+        break;
+    }
+    // Display the resulting frame
+    cv::imshow( "Frame", frame );
+
+    // Press  ESC on keyboard to exit
+    char c=(char)cv::waitKey(25);
+    if(c==27)
+        break;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    cv::destroyAllWindows();
+
+    return 0;
+  }
